80e0b2844f83438d9ed6a7f8d244a5ef362dddb0
fdaindia <admin@fdaindia.org>
2024-07-31 11:24:15 +0530

    first commit

---
+++ src/main/java/com/fdaindia/demo/FdaIndiaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fdaindia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdaIndiaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdaIndiaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdaindia/demo/FdaIndiaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdaindia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdaIndiaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
